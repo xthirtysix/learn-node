e484af6876b32ec40495e00cc10fc66c9710101e
x36 <ggalliosha@gmail.com>
2022-08-19 15:46:04 +0300

    Solved multithread performance task

--- multithread/01-multithread-performance-task.js
+++ multithread/01-multithread-performance-task.js
@@ -13,16 +13,38 @@ const performanceObserver = new PerformanceObserver((items) => {
 performanceObserver.observe({ entryTypes: ['measure'] });
 
 const workerFunction = (array) => {
+    return new Promise((resolve, _reject) => {
+        performance.mark('worker start');
 
+        const worker = new Worker('./worker', {
+            workerData: { array },
+        });
+        worker.on('message', (msg) => {
+            performance.mark('worker end');
+            performance.measure('worker', 'worker start', 'worker end');
+            resolve(msg);
+        });
+    });
 };
 
 const forkFunction = (array) => {
+    return new Promise((resolve, _reject) => {
+        performance.mark('fork start');
 
+        const forkProcess = fork('./fork');
+        forkProcess.send({array});
+        forkProcess.on('message', (msg) => {
+            resolve(msg);
+            performance.mark('fork end');
+            performance.measure('fork', 'fork start', 'fork end');
+        });
+    });
 };
 
 const main = async () => {
     try {
-
+        await workerFunction([25, 19, 48, 30]);
+        await forkFunction([25, 19, 48, 30]);
     } catch (e) {
         console.error(e.message);
     }
